61d091bca326f9c86117b08ddfc9d4a2ec38ce80
Marshal Wu <marshal.wu@gmail.com>
2013-05-16 16:11:39 +0800

    实现绘制三角形并着色的功能

---
+++ src/com/example/opengl/Mesh.java
@@ -0,0 +1,68 @@
+package com.example.opengl;
+
+import android.content.Context;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import static android.opengl.GLES20.*;
+import static android.opengl.GLES20.GL_TRIANGLE_STRIP;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marshal
+ * Date: 13-5-16
+ * Time: 下午3:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Mesh {
+
+    private Shader shader;
+
+    private FloatBuffer vertexBuffer;
+
+    public Mesh(Context context) {
+        shader = new Shader();
+        shader.setProgram(context, R.raw.vertexes, R.raw.fragment);
+
+        vertexBuffer = ByteBuffer.allocateDirect(3 * 4 * 3).order(ByteOrder.nativeOrder()).asFloatBuffer();
+    }
+
+    public void draw() {
+        setVertexBuffer();
+
+        this.shader.useProgram();
+
+        //获取shader的aPosition变量“指针”
+        int aPosition = this.shader.getHandle("aPosition");
+        //给Shader中aPosition变量赋值（顶点缓冲）
+        glVertexAttribPointer(aPosition, 3, GL_FLOAT, false,
+                3 * 4, vertexBuffer);
+        glEnableVertexAttribArray(aPosition);
+
+        //绘制三角形
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+    /**
+     * 设置顶点缓冲
+     */
+    private void setVertexBuffer(){
+        vertexBuffer.clear();
+        vertexBuffer.put(createTriangleVertexes());
+        vertexBuffer.position(0);
+    }
+
+    /**
+     * 创建一个三角形的各顶点坐标
+     * @return
+     */
+    private float[] createTriangleVertexes() {
+        return new float[]{
+                -0.5f, 0.5f, 0,
+                -0.5f, -0.5f, 0,
+                0.5f, -0.5f, 0
+        };
+    }
+}
--- src/com/example/opengl/MyActivity.java
+++ src/com/example/opengl/MyActivity.java
@@ -11,7 +11,12 @@ import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.*;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements GLSurfaceView.Renderer {
+
+    private GLSurfaceView surfaceView;
+
+    private Mesh mesh;
+
     /**
      * Called when the activity is first created.
      */
@@ -19,27 +24,29 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        GLSurfaceView surfaceView = new GLSurfaceView(this);
-        surfaceView.setRenderer(new GLSurfaceView.Renderer() {
-            @Override
-            public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-                int color = Color.BLUE;
-                glClearColor(Color.red(color), Color.green(color), Color.blue(color), 1);
-            }
-
-            @Override
-            public void onSurfaceChanged(GL10 gl10, int width, int height) {
-                //TODO 界面变化时（比如屏幕从竖屏到横屏）调用
-            }
-
-            @Override
-            public void onDrawFrame(GL10 gl10) {
-                Log.d("OpenGLTutorial", ">>>>on draw frame");
-                glClear(GL_COLOR_BUFFER_BIT);
-            }
-        });
+        surfaceView = new GLSurfaceView(this);
+        //设置使用opengl es2，默认是1.x
+        surfaceView.setEGLContextClientVersion(2);
+        surfaceView.setRenderer(this);
 
         surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         setContentView(surfaceView);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        glClearColor(0, 0, 0, 0);
+        mesh=new Mesh(this);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        mesh.draw();
     }
 }
