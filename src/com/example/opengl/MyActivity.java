2d9ab0d5b1b2973114fc3e392f6e2652d9a48f93
Marshal Wu <marshal.wu@gmail.com>
2013-05-22 14:58:23 +0800

    最基本的（代码比较混乱）分片动画

--- src/com/example/opengl/MyActivity.java
+++ src/com/example/opengl/MyActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -20,9 +22,12 @@ import static android.opengl.GLES20.*;
 
 public class MyActivity extends Activity implements GLSurfaceView.Renderer {
 
+    //从左到右
+    private int[] partInfo, partInfo1;
+
     private GLSurfaceView surfaceView;
 
-    private Mesh mesh;
+    private List<Mesh> meshes = new ArrayList<Mesh>();
 
     private float ratio;
 
@@ -32,9 +37,13 @@ public class MyActivity extends Activity implements GLSurfaceView.Renderer {
 
     private ViewGroup rootView;
 
-    private long duration = 1000;
+    private long duration = 15000;
 
     private boolean startAnimation;
+
+    private int width, height;
+
+    private float w;
 
     /**
      * Called when the activity is first created.
@@ -72,7 +81,6 @@ public class MyActivity extends Activity implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
         glClearColor(0, 0, 0, 0);
-        mesh = new Mesh(this);
     }
 
     @Override
@@ -80,27 +88,75 @@ public class MyActivity extends Activity implements GLSurfaceView.Renderer {
         glViewport(0, 0, width, height);
         ratio = width / (float) height;
         Matrix.orthoM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, -10f, 10f);
+
+        this.width = width;
+        this.height = height;
+
+        //这么做是为了尽量不出小数，而且符合实际使用情况
+        partInfo = new int[]{
+                150, 200, 150, width - (150 + 200 + 150)
+        };
+
+        //放置各个“片”到左侧的偏移量
+        partInfo1 = new int[partInfo.length];
+
+        for (int i = 0, j = 0; i < partInfo1.length; i++) {
+            partInfo1[i] = j;
+            j += partInfo[i];
+        }
+
+        meshes.clear();
+
+        for (int i = 0; i < partInfo.length; i++) {
+            meshes.add(new Mesh(this));
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
         if (startAnimation) {
-            mesh.draw(projectionMatrix);
+            for (Mesh mesh : meshes) {
+                mesh.draw(projectionMatrix);
+            }
         }
     }
 
     private void startAnimation() {
+        w = ratio * 2 * (partInfo.length - 1) + partInfo[0] / (width / ratio / 2);
         startAnimation = true;
         ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
         animator.setDuration(duration);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                final float factor = (Float) valueAnimator.getAnimatedValue();
                 surfaceView.queueEvent(new Runnable() {
                     @Override
                     public void run() {
-                        mesh.setVertexBuffer(createVertexes());
+                        for (int i = 0; i < partInfo.length; i++) {
+                            Mesh mesh = meshes.get(i);
+                            float left = -ratio + partInfo1[i] / (width / ratio / 2);
+                            float right = left + partInfo[i] / (width / ratio / 2);
+                            float[] vertexes = new float[]{
+                                    left, 1, 0,
+                                    left, -1, 0,
+                                    right, 1, 0,
+                                    right, -1, 0
+                            };
+                            //坐标x轴左移的终点
+                            float move = ratio * 2 * (partInfo.length - 1 - i) +
+                                    partInfo[i] / (width / ratio / 2) +
+                                    partInfo1[i] / (width / ratio / 2);
+
+                            for (int j = 0; j < vertexes.length; j += 3) {
+                                if (w * factor < move) {
+                                    vertexes[j] = vertexes[j] - move + w * factor;
+                                }
+                            }
+                            mesh.setVertexBuffer(vertexes);
+                        }
+
                         surfaceView.requestRender();
                     }
                 });
@@ -117,19 +173,23 @@ public class MyActivity extends Activity implements GLSurfaceView.Renderer {
                                 targetView.setVisibility(View.INVISIBLE);
                             }
                         }
-                        , 100);
+                        , 80);
             }
 
             @Override
             public void onAnimationEnd(Animator animator) {
                 targetView.setVisibility(View.VISIBLE);
                 startAnimation = false;
+
+                surfaceView.requestRender();
             }
 
             @Override
             public void onAnimationCancel(Animator animator) {
                 targetView.setVisibility(View.VISIBLE);
                 startAnimation = false;
+
+                surfaceView.requestRender();
             }
 
             @Override
@@ -139,30 +199,24 @@ public class MyActivity extends Activity implements GLSurfaceView.Renderer {
 
         //创建视图的截图
         rootView.setDrawingCacheEnabled(true);
-        Bitmap bitmap = rootView.getDrawingCache();
-        final Bitmap texture = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth() / 2,
-                bitmap.getHeight());
+        final Bitmap bitmap = Bitmap.createBitmap(rootView.getDrawingCache());
 
         rootView.setDrawingCacheEnabled(false);
 
         surfaceView.queueEvent(new Runnable() {
             @Override
             public void run() {
-                mesh.loadTexture(texture);
-                texture.recycle();
+                //为每个Mash设置Texture
+                for (int i = 0; i < partInfo.length; i++) {
+                    Bitmap texture = Bitmap.createBitmap(bitmap, partInfo1[i], 0, partInfo[i],
+                            bitmap.getHeight());
+                    meshes.get(i).loadTexture(texture);
+                    texture.recycle();
+                }
+                bitmap.recycle();
             }
         });
 
         animator.start();
     }
-
-    private float[] createVertexes() {
-        return new float[]{
-                -ratio, 1, 0,
-                -ratio, -1, 0,
-                0, 1, 0,
-                0, -1, 0
-        };
-    }
-
 }
