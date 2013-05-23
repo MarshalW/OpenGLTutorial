8044af968e160bf61c3497bef48833300a5af4eb
Marshal Wu <marshal.wu@gmail.com>
2013-05-23 15:21:11 +0800

    最简单的透视投影

--- src/com/example/opengl/MyActivity.java
+++ src/com/example/opengl/MyActivity.java
@@ -1,22 +1,43 @@
 package com.example.opengl;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
-import android.graphics.Point;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-public class MyActivity extends Activity {
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
-    private View targetView;
+import static android.opengl.GLES20.*;
+
+public class MyActivity extends Activity implements GLSurfaceView.Renderer {
 
     private ViewGroup rootView;
 
-    private SegmentViewController controller;
+    private GLSurfaceView surfaceView;
+
+    private float ratio;
+
+    private int width, height;
+
+    private Mesh mesh;
+
+    //投影矩阵
+    private float[] projectionMatrix = new float[16];
+
+    //模型矩阵
+    private float[] modelMatrix = new float[16];
+
+    //视图矩阵
+    private float[] viewMatrix = new float[16];
+
+    //模型视图投影矩阵
+    private float[] mvpMatrix = new float[16];
 
     /**
      * Called when the activity is first created.
@@ -26,43 +47,91 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        targetView = findViewById(R.id.targetView);
         rootView = (ViewGroup) findViewById(R.id.rootView);
 
-        controller = new SegmentViewController(this, rootView, targetView);
-        controller.setDuration(1000);
-        controller.setAnimatorListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                Toast.makeText(MyActivity.this, "动画完成", Toast.LENGTH_SHORT).show();
-            }
-        });
+        surfaceView = new GLSurfaceView(this);
+        surfaceView.setEGLContextClientVersion(2);
 
-        //为了测试，获取屏幕的宽度，正式情况下应该是动态获取界面宽度
-        Point point = new Point();
-        getWindowManager().getDefaultDisplay().getSize(point);
-        final int displayWidth = point.x;
+        //设置背景透明
+        surfaceView.setEGLConfigChooser(8, 8, 8, 8, 0, 0);
+        surfaceView.setZOrderOnTop(true);
+        surfaceView.getHolder().setFormat(PixelFormat.TRANSPARENT);
 
-        targetView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                controller.setSegmentsData(new int[]{
-                        150, 200, 150, displayWidth - (150 + 200 + 150)
-                });
-                controller.startAnimation();
-            }
-        });
+        surfaceView.setRenderer(this);
+        surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        rootView.addView(surfaceView);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        controller.onResume();
     }
 
     @Override
     protected void onPause() {
-        controller.onPause();
         super.onPause();
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        glClearColor(0, 0, 0, 0);
+
+        //照相机位置
+        float eyeX = 0.0f;
+        float eyeY = 0.0f;
+        float eyeZ = 7;
+
+        //照相机拍照方向
+        float lookX = 0.0f;
+        float lookY = 0.0f;
+        float lookZ = -1.0f;
+
+        //照相机的垂直方向
+        float upX = 0.0f;
+        float upY = 1.0f;
+        float upZ = 0.0f;
+
+        Matrix.setLookAtM(viewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+
+        Bitmap texture = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ta));
+        mesh = new Mesh(this);
+        mesh.loadTexture(texture);
+        texture.recycle();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        this.width = width;
+        this.height = height;
+        glViewport(0, 0, width, height);
+        ratio = width / (float) height;
+
+        float left = -ratio;
+        float right = ratio;
+        float top = 1;
+        float bottom = -1;
+        float near = 7;
+        float far = 100;
+        Matrix.frustumM(projectionMatrix, 0, left, right, bottom, top, near, far);
+
+        mesh.setVertexBuffer(new float[]{
+                -ratio, 1, 0,
+                -ratio, -1, 0,
+                ratio, 1, 0,
+                ratio, -1, 0
+        });
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        Matrix.setIdentityM(modelMatrix, 0);
+        Matrix.multiplyMM(mvpMatrix, 0, viewMatrix, 0, modelMatrix, 0);
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, mvpMatrix, 0);
+
+        mesh.draw(mvpMatrix);
     }
 }
